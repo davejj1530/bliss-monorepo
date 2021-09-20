4c52b60accb66524753052ff3b1240426e2f4a52
davidceriola <davejjceriola@gmail.com>
2021-09-20 20:58:47 +0800

    test api route

--- packages/client-docs/pages/index.tsx
+++ packages/client-docs/pages/index.tsx
@@ -2,10 +2,23 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Button, Section, Space } from "bliss-core";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState(null);
+
+  const queryApi = useCallback(async () => {
+    const res = await fetch("/api/hello");
+    const data = await res.json();
+    setName(data.name);
+  }, []);
+
+  useEffect(() => {
+    queryApi();
+  }, [queryApi]);
   return (
     <>
+      {name && <div>API: {name}</div>}
       <Space>
         <Button onClick={() => alert("HELLO WORLD!")} pill>
           HELLO WORLD!
